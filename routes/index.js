bb7d2a357c17ff4c066ae005e3b36f05b09b00ea
Jonatan <jonatan.sonar@gmail.com>
2015-06-14 20:57:48 +0200

    ejercicio modulo 6

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Author', autor: 'Jonatan González Villena' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
